5b8d59854611d415d41eaff17d1faa07b6c27fb2
n <2585803+tt5@users.noreply.github.com>
2023-10-22 15:27:38 +0200

    first commit

---
+++ src/components/Button.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let bg = 'white';
+  export let text = 'black';
+</script>
+
+<button style="background: {bg}; color: {text}">
+  <slot />
+</button>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+  import Button from '$c/Button.svelte'
+</script>
+
+<h1>hello</h1>
+<Button bg="red">Red Button</Button>
