54ab5d3cd6b5c4bf3d7e18478dcde6a7a5f4f2f4
n <2585803+tt5@users.noreply.github.com>
2024-05-28 02:54:10 +0200

    tailwindcss

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
+<script>
+  import "../app.css";
+</script>
+
 <nav>
-<a href="/">Home</a>
-<a href="/one" data-sveltekit-prefetch>One</a>
-<a href="/two" data-sveltekit-prefetch>Two</a>
+  <a href="/">Home</a>
+  <a href="/one" data-sveltekit-prefetch>One</a>
+  <a href="/two" data-sveltekit-prefetch>Two</a>
 </nav>
-<slot />
+<slot></slot>
+
+<style></style>
+
